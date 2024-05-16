dfabfe5117d9bda73c7a4337504466646c838174
sebastian <sebastian.seitalahti@gmail.com>
2024-05-16 22:29:41 +0300

    update env, add config matcher

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,9 @@ const nextConfig = {
   env: {
     GOOGLE_APP_CLIENT_ID: process.env.GOOGLE_APP_CLIENT_ID,
     GOOGLE_APP_CLIENT_SECRET: process.env.GOOGLE_APP_CLIENT_SECRET,
-    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    AUTH_SECRET: process.env.AUTH_SECRET,
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    AUTH_TRUST_HOST: process.env.AUTH_TRUST_HOST,
     BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
   },
 };
