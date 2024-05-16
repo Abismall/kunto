0255474cddec3d20f932cb24cbfde1ce122dd591
sebastian <sebastian.seitalahti@gmail.com>
2024-05-16 22:55:27 +0300

    trash middleware

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
+"use client"
 import BlogSection from "./_components/blog-section";
 import FAQSection from "./_components/faq-section";
 import HeroSection from "./_components/hero-section";
+import { useRouter } from 'next/navigation'
+import { useSession } from "next-auth/react"
+import { useEffect } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const session = useSession();
+
+  useEffect(() => {
+    if (session.status == "unauthenticated") {
+      router.push("/api/auth/signin")
+
+    }
+  }, [session, router])
+  if (session.status == "loading" || session.status == "unauthenticated") return <div><p>Ladataan...</p></div>
   return (
     <main >
-        <HeroSection />
-        <FAQSection/>
-        <BlogSection/>
+      <HeroSection />
+      <FAQSection />
+      <BlogSection />
     </main>
   );
 }
