5ebef662195e657e2cc8a2d17333facc64112d12
sebastian <sebastian.seitalahti@gmail.com>
2024-06-01 13:16:55 +0300

    rename useState

--- app/_components/hero.tsx
+++ app/_components/hero.tsx
@@ -4,10 +4,10 @@ import React, { useEffect, useState } from "react";
 import Link from "next/link";
 
 export default function Hero() {
-  const [isFirstLoad, setIsFirstLoad] = useState(true);
+  const [runAnimation, setrunAnimation] = useState(false);
 
   useEffect(() => {
-    setIsFirstLoad(false);
+    setrunAnimation(true);
   }, []);
 
   return (
@@ -23,18 +23,18 @@ export default function Hero() {
       <div className="container flex px-5 py-24 pt-56 md:flex-row flex-col items-center justify-center md:justify-start">
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-14 md:mb-0 items-center text-center">
           <h1
-            className={`sm:text-2xl text-3xl lg:text-8xl mb-4 text-secondary ${isFirstLoad ? "animate-slideInRight" : ""
+            className={`sm:text-2xl text-3xl lg:text-8xl mb-4 text-secondary ${runAnimation ? "animate-slideInRight" : ""
               }`}
           >
             Anna kuntosi kohota
           </h1>
           <p
-            className={`mb-10 leading-relaxed lg:text-2xl opacity-0 ${isFirstLoad ? "animate-slideDown" : ""
+            className={`mb-10 leading-relaxed lg:text-2xl opacity-0 ${runAnimation ? "animate-slideDown" : ""
               }`}
             style={{
-              animationDelay: isFirstLoad ? "0.75s" : "0s",
+              animationDelay: runAnimation ? "0.75s" : "0s",
               animationFillMode: "forwards",
-              opacity: isFirstLoad ? "0" : "1",
+              opacity: runAnimation ? "0" : "1",
             }}
           >
             Tilaa personalisoitu treeniohjelma ja saavuta paras itsesi!
@@ -42,24 +42,24 @@ export default function Hero() {
           <div className="flex justify-center md:justify-start gap-6">
             <Link
               href="#service-info-block "
-              className={`inline-flex text-#333333 border border-secondary py-2 px-6 md:py-3 md:px-8 focus:outline-none rounded text-lg md:text-3xl opacity-0 ${isFirstLoad ? "animate-slideInLeft" : ""
+              className={`inline-flex text-#333333 border border-secondary py-2 px-6 md:py-3 md:px-8 focus:outline-none rounded text-lg md:text-3xl opacity-0 ${runAnimation ? "animate-slideInLeft" : ""
                 } hover:bg-secondary hover:text-white transition-colors duration-500 ease-in-out`}
               style={{
-                animationDelay: isFirstLoad ? "0.8s" : "0s",
+                animationDelay: runAnimation ? "0.8s" : "0s",
                 animationFillMode: "forwards",
-                opacity: isFirstLoad ? "0" : "1",
+                opacity: runAnimation ? "0" : "1",
               }}
             >
               <p className="text-center">Palvelut</p>
             </Link>
             <Link
               href="/contact"
-              className={`inline-flex text-#333333 border border-secondary py-2 px-6 md:py-3 md:px-8 focus:outline-none rounded text-lg md:text-3xl opacity-0 ${isFirstLoad ? "animate-slideInRight" : ""
+              className={`inline-flex text-#333333 border border-secondary py-2 px-6 md:py-3 md:px-8 focus:outline-none rounded text-lg md:text-3xl opacity-0 ${runAnimation ? "animate-slideInRight" : ""
                 } hover:bg-secondary hover:text-white transition-colors duration-500 ease-in-out`}
               style={{
-                animationDelay: isFirstLoad ? "0.8s" : "0s",
+                animationDelay: runAnimation ? "0.8s" : "0s",
                 animationFillMode: "forwards",
-                opacity: isFirstLoad ? "0" : "1",
+                opacity: runAnimation ? "0" : "1",
               }}
             >
               <p className="text-center">Yhteystiedot</p>
